7273264bbc81b41bedbf4cb9e5e4c122108c9374
Sofia Ford <scfordw@bu.edu>
2025-12-01 17:49:32 -0500

    Creating header component to begin styling website and create functionality

---
+++ app/components/Header.tsx
@@ -0,0 +1,26 @@
+"use client"
+import styled from "styled-components";
+
+const Div = styled.div`
+    display: flex;
+    align-items: flex-start;
+    background-color: var(--color-sky-blue);
+    margin: 0 auto;
+    padding: 3%;
+`
+
+const Title = styled.h1`
+    color: var(--color-black);
+    font-family: var(--font-playfair), serif;
+    font-style: italic;
+    font-weight: 900;
+    font-size: calc(2px + 2vw);
+`
+
+export default function Header(){
+    return(
+        <Div>
+            <Title>MP-6: OAuth Application</Title>
+        </Div>
+    )
+}
